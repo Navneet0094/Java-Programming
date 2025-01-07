4ef2c68dfa9073593fa45fe5f9fc7dfa71a4fa6e
Navneet <s.navneet@op.iitg.ac.in>
2025-01-08 00:08:12 +0530

    Add files via upload

---
+++ Number Patterns/Rectangleinc.java
@@ -0,0 +1,27 @@
+class Rectangleinc{
+//     public static void main(String[] args) {
+//         for(int  i = 1 ; i<=5;i++){
+
+//             for(int  j = 1 ; j<=5;j++){
+//                 System.out.print(j+i-1+" ");
+                                
+//             }   
+//         System.out.println("");        
+//         }           
+//     }
+
+// }
+
+public static void main(String[] args) {
+    for(int  i = 1 ; i<=5;i++){
+        int k =i;
+        for(int  j = 1 ; j<=5;j++){
+            System.out.print(k);
+            k++;
+        }
+    System.out.println();
+
+    }
+    
+}
+}
